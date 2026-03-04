075875c2fe20b4b39a7609d7bfaa720d20f142cc
hqkqne <hqkqne@gmail.com>
2026-03-04 15:54:52 +0300

    models +transactions

--- src/models.py
+++ src/models.py
@@ -1,10 +1,10 @@
 import datetime
-from datetime import DateTime
+from sqlalchemy import (
+func, DateTime, String, ForeignKey, CheckConstraint)
 from time import timezone
 
 from sqlalchemy.orm import (
 DeclarativeBase, Mapped, mapped_column, relationship)
-from sqlalchemy import ForeignKey, String
 class Base(DeclarativeBase):
     pass
 
@@ -13,12 +13,14 @@ class User(Base):
     id: Mapped[int] = mapped_column(
         primary_key=True, autoincrement=True)
     full_name: Mapped[str] = mapped_column(String(20))
-    #created_at: Mapped[] = mapped_column()
-
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
     account = relationship("Account", back_populates= "User")
 
 class Account(Base):
-    __tablename__ = "accounts"
+    __tablename__ = 'accounts'
     id: Mapped[int] = mapped_column(
         primary_key=True, autoincrement= True)
     balance: Mapped[int] = mapped_column()
@@ -26,16 +28,34 @@ class Account(Base):
 
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id", ondelete="CASCADE"))
+    ## relationship
+    user = relationship("User", back_populates= "account")
+    #transactions
+    out_transactions = relationship("Transaction", back_populates="from_account")
+    in_transactions = relationship("Transaction", back_populates="to_account")
 
-    user = relationship("User", back_populates= "accounts")
-    
-    class Transaction(Base):
-        __tablename__ = "transactions"
-        id: Mapped[int] = mapped_column(
-            primary_key=True, autoincrement=True)
-        from_account_id: Mapped[...] = mapped_column(ForeignKey("accounts.id", ondelete="CASCADE"))
-        to_account_id: Mapped[...] = mapped_column(ForeignKey("accounts.id", ondelete="CASCADE"))
-        status: Mapped[str] = mapped_column(...)
-        created_at: Mapped[datetime.datetime] =(
-            mapped_column(DateTime(timezone = True),
-                          server_default= func.now()))#date_time
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True)
+    from_account_id: Mapped["Account"] = mapped_column(ForeignKey("accounts.id", ondelete="CASCADE"))
+    to_account_id: Mapped["Account"] = mapped_column(ForeignKey("accounts.id", ondelete="CASCADE"))
+    status: Mapped[str] = mapped_column(String(15))
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+    from_account = relationship(
+        "Account",
+        back_populates= "transactions"
+    )
+    to_account = relationship(
+        "Account",
+        back_populates="transactions"
+    )
+
+    # Limitations
+    __table_args__ = (
+        CheckConstraint("from_account_id != to_account_id", name = "check_no_self_transfer"),
+        CheckConstraint("amount > 0", name = "check_positive_amount")
+    )
