3082d911089dd0de13e400a11c0b23706279e6c3
hqkqne <hqkqne@gmail.com>
2026-03-06 22:29:45 +0300

    wip:models edits

--- src/models.py
+++ src/models.py
@@ -1,8 +1,7 @@
 import datetime
 from sqlalchemy import (
-func, DateTime, String, ForeignKey, CheckConstraint)
-from time import timezone
-
+func, DateTime, String, ForeignKey, CheckConstraint, Numeric)
+from decimal import Decimal
 from sqlalchemy.orm import (
 DeclarativeBase, Mapped, mapped_column, relationship)
 class Base(DeclarativeBase):
@@ -23,22 +22,21 @@ class Account(Base):
     __tablename__ = 'accounts'
     id: Mapped[int] = mapped_column(
         primary_key=True, autoincrement= True)
-    balance: Mapped[int] = mapped_column()
-    status: Mapped[str]
+    balance: Mapped[Decimal] = mapped_column(Numeric(15,2), nullable= False)
+    currency: Mapped[str] = mapped_column(String(3), default= "USD")
+    status: Mapped[str] = mapped_column(default= "...", nullable=False)
 
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id", ondelete="CASCADE"))
     ## relationship
     user = relationship("User", back_populates= "account")
     #transactions
-    out_transactions = relationship("Transaction", back_populates="from_account")
-    in_transactions = relationship("Transaction", back_populates="to_account")
+    outgoing_transactions = relationship("Transaction", back_populates="from_account")
+    incoming_transactions = relationship("Transaction", back_populates="to_account")
 
 class Transaction(Base):
     __tablename__ = "transactions"
     id: Mapped[int] = mapped_column(
         primary_key=True, autoincrement=True)
-    from_account_id: Mapped["Account"] = mapped_column(ForeignKey("accounts.id", ondelete="CASCADE"))
+    from_account_id: Mapped["Account"] = mapped_column(ForeignKey("accounts.id", ondelete="SET NULL"))
     to_account_id: Mapped["Account"] = mapped_column(ForeignKey("accounts.id", ondelete="CASCADE"))
     status: Mapped[str] = mapped_column(String(15))
     created_at: Mapped[datetime.datetime] = mapped_column(
@@ -59,6 +57,7 @@ class Transaction(Base):
         CheckConstraint("from_account_id != to_account_id", name = "check_no_self_transfer"),
         CheckConstraint("amount > 0", name = "check_positive_amount")
     )
+
     class Outbox(Base):
         __tablename__ = "outbox"
         id: Mapped[int] = mapped_column(
